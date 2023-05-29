4fe6d5b9bdb11769dfa7663740874c46a551fc4f
Lincoln Gibson <lincolngibson7@gmail.com>
2023-05-29 14:39:16 -0300

    Add react app, and deleted unwanted files.

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Math magicians</header>
+    </div>
+  );
+}
+
+export default App;
