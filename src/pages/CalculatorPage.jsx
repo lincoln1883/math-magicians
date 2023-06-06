999991d6b751cce6f7082aa4c8039187468efcd9
Lincoln Gibson <lincolngibson7@gmail.com>
2023-06-06 18:02:38 -0300

    Add calculator page.

---
+++ src/pages/CalculatorPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Calculator from '../components/calculator';
+
+const CalculatorPage = () => (
+  <div className="wrapper">
+    <h1>Let&apos;s do some math</h1>
+    <Calculator />
+  </div>
+);
+
+export default CalculatorPage;
