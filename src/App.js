47be2f778b55164fd7ef9587d417ab36f85739c8
Lincoln Gibson <lincolngibson7@gmail.com>
2023-06-06 18:00:51 -0300

    Add routes and import router  for the pages.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
-import Calculator from './components/calculator';
+import { Route, Routes } from 'react-router-dom';
+import CalculatorPage from './pages/CalculatorPage';
 import Quotes from './components/quotes';
+import Layout from './components/Layout';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Calculator />
-      <Quotes />
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="quotes" element={<Quotes />} />
+          <Route path="calculator" element={<CalculatorPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
