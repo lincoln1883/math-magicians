0d577e919b7c0cd356768f719dcf822ac2c6bf9b
Lincoln Gibson <lincolngibson7@gmail.com>
2023-06-06 18:03:00 -0300

    Add home page.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="text-container">
+    <h1>Welcome to our page!</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum eum
+      provident totam error magnam fuga hic voluptate animi, recusandae quod
+      minus quasi enim? Natus ad non laudantium dolorem libero ea!
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum quidem
+      fuga sit expedita, nam enim quibusdam omnis soluta vel ut perspiciatis
+      corporis facilis ipsam quasi! Repellendus error praesentium beatae aut!
+    </p>
+  </div>
+);
+
+export default Home;
