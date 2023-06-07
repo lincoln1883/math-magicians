8588c562d4d88af8de888dc453ba6ada695f96fa
Lincoln Gibson <lincolngibson7@gmail.com>
2023-06-07 19:03:33 -0300

    Rename the class to classes.

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,7 +6,7 @@ const Button = ({ button, handleClick }) => (
     <button
       type="button"
       key={button.id}
-      className={button.class}
+      className={button.classes}
       onClick={handleClick}
     >
       {button.name}
@@ -18,7 +18,7 @@ Button.propTypes = {
   button: PropTypes.shape({
     id: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
-    class: PropTypes.string.isRequired,
+    classes: PropTypes.string.isRequired,
   }).isRequired,
 };
 export default Button;
